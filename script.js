7cff7ce51e4604377a645a40beb33c617e252f6b
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-05-15 10:03:29 -0400

    Score functionality created

--- script.js
+++ script.js
@@ -23,41 +23,67 @@ if cemetery is clicked, subtract 100 points
 keep a running tally of points
 */
 
+//Declaring Variables
+let scoreText;
+let scoreTeamOne=0;
+let scoreTeamTwo=0;
+
 //This section clicks on the various buttons for Team One and adds to the point total Team One. 
-let scoreTeamOne = 
-  document.getElementById('horseTeamOne').addEventListener("click", clickedHorseTeamOne);
+let clickOnTeamOneButtons = 
+  document.getElementById('horseTeamOne').addEventListener("click", addTenTeamOne);
 
-  function clickedHorseTeamOne() {
-    console.log ("horse team one")
-  }
-  document.getElementById('cowTeamOne').addEventListener("click", clickedCowTeamOne);
 
-  function clickedCowTeamOne() {
-    console.log ("cow team one")
-  }
-  document.getElementById('cemeteryTeamOne').addEventListener("click", clickedCemeteryTeamOne);
+  document.getElementById('cowTeamOne').addEventListener("click", subtractOneTeamOne);
+
+  document.getElementById('cemeteryTeamOne').addEventListener("click", subtractOneHundredTeamOne);
 
   function clickedCemeteryTeamOne() {
     console.log ("cemetery team one")
   }
 
 //This section clicks on the various buttons for Team Two and adds to the point total for Team Two. 
-let scoreTeamTwo = 
-  document.getElementById('horseTeamTwo').addEventListener("click", clickedHorseTeamTwo);
+let clickOnTeamTwoButtons = 
+  document.getElementById('horseTeamTwo').addEventListener("click", addTenTeamTwo);
+  
+  
+  document.getElementById('cowTeamTwo').addEventListener("click", subtractOneTeamTwo);
 
-  function clickedHorseTeamTwo() {
-    console.log ("horse team two")
-  }
-  document.getElementById('cowTeamTwo').addEventListener("click", clickedCowTeamTwo);
+  document.getElementById('cemeteryTeamTwo').addEventListener("click", subtractOneHundredTeamrTwo);
 
-  function clickedCowTeamTwo() {
-    console.log ("cow team two")
-  }
-  document.getElementById('cemeteryTeamTwo').addEventListener("click", clickedCemeteryTeamTwo);
 
-  function clickedCemeteryTeamTwo() {
-    console.log ("cemetery team two")
-  }
+//Adding to Team One Score
+function addTenTeamOne(){
+  scoreTeamOne= scoreTeamOne +10;
+  console.log(scoreTeamOne)
+};
+
+function subtractOneTeamOne(){
+  scoreTeamOne= scoreTeamOne -1;
+  console.log(scoreTeamOne)
+};
+
+function subtractOneHundredTeamOne(){
+  scoreTeamOne= scoreTeamOne -100;
+  console.log(scoreTeamOne)
+};
+
+
+//Adding to Team Two Score
+function addTenTeamTwo(){
+  scoreTeamTwo= scoreTeamTwo +10;
+  console.log(scoreTeamTwo)
+};
+
+function subtractOneTeamTwo(){
+  scoreTeamTwo= scoreTeamTwo -1;
+  console.log(scoreTeamTwo)
+};
+
+function subtractOneHundredTeamrTwo(){
+  scoreTeamTwo= scoreTeamTwo -100;
+  console.log(scoreTeamTwo)
+};
+
 
 /*congratulations
 maybe a balloon animation feature eventually
